fbf7abcf6ac9f9a6dd166cf73d0b0622bc9c0762
Jiang Sheng <gisonrg@gmail.com>
2016-08-22 23:53:39 +0800

    Update the place schema to hold more digits after decimal points for geo metrics.

--- database/migrations/2016_08_17_160948_create_places_table.php
+++ database/migrations/2016_08_17_160948_create_places_table.php
@@ -16,8 +16,8 @@ class CreatePlacesTable extends Migration
             $table->increments('id');
             $table->string('fb_id')->nullable();
             $table->string('name');
-            $table->float('lat');
-            $table->float('long');
+            $table->float('lat', 16, 12); // decimal part is guaranteed to be smaller or equal than 3 digits
+            $table->float('long', 16, 12); // decimal part is guaranteed to be smaller or equal than 3 digits
             $table->string('city')->nullable();
             $table->string('street')->nullable();
             $table->string('zip')->nullable();
