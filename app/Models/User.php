8f17d0ccb89bdbd5419666d63c2ec125a7e933e2
Jiang Sheng <gisonrg@gmail.com>
2016-08-25 20:02:25 +0800

    Include total checkins count as a meta data for users.

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,11 +8,13 @@ class User extends UuidModel {
     }
 
     public function getMetaData() {
+        $checkins = $this->checkins();
         return [
             'id' => $this->id,
             'fb_id' => $this->fb_id,
             'name' => $this->name,
-            'avatar' => $this->avatar_url
+            'avatar' => $this->avatar_url,
+            'total_checkins' => $checkins->count()
         ];
     }
 
